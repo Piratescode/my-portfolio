ac5cf115cdacea9e4ab6a393c4a58602f0829d59
Stephen Alhamdu <gen.lordrain@gmail.com>
2025-05-09 10:34:29 -0500

    Made some changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Hero from './components/Hero';  // Import Hero component
 import Navbar from './components/Navbar';
-import "./components/Navbar.css"
-import "./components/Hero.css"
+import './components/Navbar.css';
+import './components/Hero.css';
 import './App.css';  // Import styles for App
 
 const App = () => {
