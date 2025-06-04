41e17e1e8d6f4fb64495935687daf4e445948d06
Stephen Alhamdu <gen.lordrain@gmail.com>
2025-06-04 07:10:45 -0500

    Added Some changes

--- src/components/About.js
+++ src/components/About.js
@@ -4,26 +4,31 @@ import './About.css';
 
 const About = () => {
   return (
-    <section className="hero-container">
-      <div className="hero-content">
-        <h1>
-          Crafting <span className="highlight">Educational Solutions</span> with <strong>Code.</strong>
-        </h1>
-        <p>
-          I specialize in developing custom school portals and computer-based testing systems.
-          Let’s transform your educational technology with innovative and reliable solutions
-          tailored to your needs. I use cutting-edge technologies to deliver efficient and scalable
-          applications.
-        </p>
-        <div className="hero-buttons">
-          <button className="primary-btn">Portals</button>
-          <button className="outline-btn">Tests</button>
+    <section className="about">
+      <div className='containers'>
+        <div className="about-content">
+          <h1>
+            Crafting <span className="highlight">Educational Solutions</span> with <strong>Code.</strong>
+          </h1>
+          <p>
+            I specialize in developing custom school portals and computer-based testing systems.
+            Let’s transform your educational technology with innovative and reliable solutions
+            tailored to your needs. I use cutting-edge technologies to deliver efficient and scalable
+            applications.
+          </p>
+          <div className="about-buttons">
+            <button className="primary-btn">Portals</button>
+            <button className="outline-btn">Tests</button>
+          </div>
         </div>
+        
+       
       </div>
-      <div className="hero-image">
-        <img src={heroImage} alt="Developer working" />
-      </div>
+      <div className='about-image'><img src={heroImage} alt="Developer working" /></div>
+
+
     </section>
+
   );
 };
 
